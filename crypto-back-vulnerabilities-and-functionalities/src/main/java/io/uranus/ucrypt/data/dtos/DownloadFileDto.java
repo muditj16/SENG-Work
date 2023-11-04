3635be201c16fe3bca1ae875af9a9695d02a296f
afaragfarag <afarag.fcih@gmail.com>
2023-11-04 11:08:22 -0700

    init

---
+++ crypto-back-vulnerabilities-and-functionalities/src/main/java/io/uranus/ucrypt/data/dtos/DownloadFileDto.java
@@ -0,0 +1,18 @@
+package io.uranus.ucrypt.data.dtos;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.core.io.Resource;
+
+@Getter
+@Setter
+@Builder
+public class DownloadFileDto {
+
+    private String fileName;
+
+    private String contentType;
+
+    private Resource file;
+}
---
+++ crypto-back-vulnerabilities-and-functionalities/src/main/java/io/uranus/ucrypt/services/mapper/EncryptionKeyMapper.java
@@ -0,0 +1,14 @@
+package io.uranus.ucrypt.services.mapper;
+
+import io.uranus.ucrypt.api.v1.resources.CreateEncryptionKeyResource;
+import io.uranus.ucrypt.api.v1.resources.EncryptionKeyListResource;
+import io.uranus.ucrypt.data.entities.EncryptionKey;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface EncryptionKeyMapper {
+
+    EncryptionKeyListResource map(EncryptionKey encryptionKey);
+
+    EncryptionKey map(CreateEncryptionKeyResource encryptionKeyResource);
+}
