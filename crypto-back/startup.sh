f049dc3539a598172123ef592bc7678131bb04ea
itraore19 <itraore@ece.uvic.ca>
2024-04-16 13:57:50 -0700

    Update startup.sh

--- crypto-back/startup.sh
+++ crypto-back/startup.sh
@@ -1,11 +1,16 @@
-#!/usr/bin/env bash
-if ! [ -f /var/opt/mssql/.initialized ] && [ -n "$DB_NAME" ]; then
-  while ! </dev/tcp/localhost/1433 2>/dev/null; do
-    sleep 2
-  done
-  echo "Creating $DB_NAME database..."
-  /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -d master \
-    -Q "CREATE DATABASE $DB_NAME"
-  touch /var/opt/mssql/.initialized
-fi &
-/opt/mssql/bin/sqlservr
+#!/bin/bash
+   #start SQL Server
+   sh -c " 
+   
+   sleep 20s
+
+   echo 'Starting setup script'
+
+   
+   /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P \$SA_PASSWORD -Q 
+   \"CREATE DATABASE $DB_NAME\"
+
+    echo 'Finished setup script'
+    exit
+    " & 
+    exec /opt/mssql/bin/sqlservr
